d16e539922dfe0638ac78395033ba1f9ec918f0f
joao-araujoo <joao.araujoo2007@gmail.com>
2022-10-02 17:38:35 -0300

    segundo código

---
+++ aula05/aprendizado.js
@@ -0,0 +1,11 @@
+let nome = 'joão'
+let sobrenome = "araujo"
+let idade = 15
+let telefone = '12-997272631'
+
+typeof nome //deverá retornar "string"
+typeof sobrenome //deverá retornar "string"
+typeof idade //deverá retornar "number"
+typeof telefone //deverá retornar "string"
+typeof NaN //deverá retornar "number"
+typeof [] //deverá retornar "object"s
