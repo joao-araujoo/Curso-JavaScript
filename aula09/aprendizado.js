71427c8c1480c38d72e2452e8c68e45ac1e61af4
joao-araujoo <joao.araujoo2007@gmail.com>
2022-10-08 21:32:42 -0300

    sexto código

---
+++ aula09/aprendizado.js
@@ -0,0 +1,11 @@
+let elemento = prompt('Qual elemento deseja selecionar?')
+let posição = prompt('Qual a posição deste elemento?')
+let escolha = document.getElementsByTagName(elemento)[posição-1]
+
+document.write('<br><br>O elemento presenta nestas condições é: ' + escolha.innerText)
+
+let cor = prompt('Qual cor deseja atribuir a este elemento?')
+escolha.style.color = cor
+
+let cor_fundo = prompt('Qual cor de fundo deseja atribuir a este elemento?')
+escolha.style.background = cor_fundo
