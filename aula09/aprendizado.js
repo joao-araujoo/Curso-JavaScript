0035dd03ddf38baf63c0b04887913da8c9c5e758
joao-araujoo <joao.araujoo2007@gmail.com>
2022-10-09 19:11:06 -0300

    DOM por classe

--- aula09/aprendizado.js
+++ aula09/aprendizado.js
@@ -1,6 +1,8 @@
 let elemento = prompt('Qual elemento deseja selecionar?')
 let posição = prompt('Qual a posição deste elemento?')
-let escolha = document.getElementsByTagName(elemento)[posição-1]
+
+let escolha = document.getElementsByClassName(elemento)[posição-1]
+//let escolha = document.getElementsByTagName(elemento)[posição-1]
 
 document.write('<br><br>O elemento presenta nestas condições é: ' + escolha.innerText)
 
