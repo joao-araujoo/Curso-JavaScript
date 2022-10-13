6a758d7c630c06b16e8ad979d582a0420ec03442
joao-araujoo <joao.araujoo2007@gmail.com>
2022-10-13 14:25:24 -0300

    botões com hover

--- aula10/aprendizado.js
+++ aula10/aprendizado.js
@@ -3,6 +3,26 @@ let botaon2 = document.querySelector('#txtn2')
 
 botaon1.addEventListener('click', atribuirn1)
 botaon2.addEventListener('click', atribuirn2)
+botaon1.addEventListener('mouseenter', entroun1)
+botaon2.addEventListener('mouseenter', entroun2)
+botaon1.addEventListener('mouseout', saiun1)
+botaon2.addEventListener('mouseout', saiun2)
+
+function entroun1(){
+    botaon1.style.background = 'rgb(206, 184, 226)'
+}
+
+function entroun2(){
+    botaon2.style.background = 'rgb(206, 184, 226)'
+}
+
+function saiun1(){
+    botaon1.style.background = 'white'
+}
+
+function saiun2(){
+    botaon2.style.background = 'white'
+}
 
 let n1 = 0
 let n2 = 0
