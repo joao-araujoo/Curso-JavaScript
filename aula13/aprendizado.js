5eed0511be5fda6b795bf45ae64d2b8e9b708d8f
joao-araujoo <joao.araujoo2007@gmail.com>
2022-10-18 15:00:36 -0300

    fixação

---
+++ aula13/aprendizado.js
@@ -0,0 +1,12 @@
+let contagemRegressiva = 10
+let felizAnoNovo = false
+
+while(contagemRegressiva >= 1){
+    console.log(`Contagem regressiva: ${contagemRegressiva}`)
+    
+    if(contagemRegressiva == 1){
+        console.log('Feliz ano novo!')
+    }
+
+    contagemRegressiva--
+}
