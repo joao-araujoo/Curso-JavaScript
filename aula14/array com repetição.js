e9ca88eea3e86d91e18528b2818f5927e45b52d8
joao-araujoo <joao.araujoo2007@gmail.com>
2022-10-20 17:11:47 -0300

    descobrindo arrays

---
+++ aula14/array com repetição.js
@@ -0,0 +1,8 @@
+let vetor = [ , , , ]
+for(let x = 0; x <= 3; x++){
+    vetor[x] = prompt(`Digite o ${x + 1}° coiso: `)
+} 
+
+for(let x = 0; x <= 3; x++){
+    alert(`Como ${x + 1}° termo, você digitou: ${vetor[x]}`)
+} 
