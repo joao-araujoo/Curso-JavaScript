2b5c9b2b181c9b64f24d6e988a3af1d25239b15e
joao-araujoo <joao.araujoo2007@gmail.com>
2022-10-22 14:13:01 -0300

    concluindo o código

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -5,14 +5,20 @@ function contar(){
     let txtinicio = document.querySelector('input#txt-inicio')
     let txtfim = document.querySelector('input#txt-fim')
     let txtpasso = document.querySelector('input#txt-passo')
-    let inicio = Number(txtinicio.value)
-    let fim = Number(txtfim.value)
-    let passo = Number(txtpasso.value)
-    let resultado = document.querySelector('div#resultado')
-    for(let x = inicio; x <= fim; x += passo){
-        resultado.innerHTML += `${x} 👉` 
-        if(x >= fim || x + passo > fim){
-            resultado.innerHTML += `✔`
-        }
+
+    if(txtinicio != '' && txtfim != '' && txtpasso != ''){
+        let inicio = Number(txtinicio.value)
+        let fim = Number(txtfim.value)
+        let passo = Number(txtpasso.value)
+        let resultado = document.querySelector('div#resultado')
+        resultado.innerHTML = ''
+        for(let x = inicio; x <= fim; x += passo){
+            resultado.innerHTML += `${x} 👉` 
+            if(x >= fim || x + passo > fim){
+                resultado.innerHTML += `✔`
+            }
+        } 
+    } else{
+        alert('[ERRO] Digite todos os campos')
     }
 }
