548fefc13bb552ad419d62157e414d29d114ce14
joao-araujoo <joao.araujoo2007@gmail.com>
2022-10-22 17:49:18 -0300

    criando JS

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,21 @@
+let botao = document.querySelector('input#botao')
+botao.addEventListener('click', tabuada)
+
+function tabuada(){
+    let txtn = document.querySelector('input#txtn')
+    let tab = document.querySelector('select#seltab')
+
+    if(txtn.value.length == 0){
+        alert('[ERRO] Insira um número!')
+    } else{
+        let n = Number(txtn.value)
+        let x = 1
+        tab.innerHTML = ''
+        while(x <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${x} = ${n * x}`
+            tab.appendChild(item)
+            x++
+        }
+    }
+}
