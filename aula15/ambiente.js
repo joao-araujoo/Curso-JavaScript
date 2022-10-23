3606bab435673be2fadd3874de865ee8407b8077
joao-araujoo <joao.araujoo2007@gmail.com>
2022-10-23 14:34:38 -0300

    criar / acessar vetores

---
+++ aula15/ambiente.js
@@ -0,0 +1,11 @@
+let num = [6, 2, 4, 9, 7]
+
+console.log(num)
+console.log(`O nosso vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é: ${num[0]}`)
+
+num[5] = 1
+num.push(5)
+
+num.sort()
+console.log(`O vetor em ordem crescente é: ${num}`)
