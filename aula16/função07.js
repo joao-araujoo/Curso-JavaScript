814dd816a9590e54b06e1aace4f7a0b4f9e10567
joao-araujoo <joao.araujoo2007@gmail.com>
2022-10-24 15:24:27 -0300

    função 7

---
+++ aula16/função07.js
@@ -0,0 +1,12 @@
+function somatório(n){
+    let s = 0
+    for(let x = 1; x <= n; x++){
+        s += x
+    }
+    return s
+}
+
+//let número = 7
+//console.log(somatório(número))
+
+console.log(somatório(5))
