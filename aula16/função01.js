a0fa72d89edcce676555676cd968438901375bc0
joao-araujoo <joao.araujoo2007@gmail.com>
2022-10-24 15:00:47 -0300

    função 1

---
+++ aula16/função01.js
@@ -0,0 +1,11 @@
+function parimpar(n){
+    if(n%2==0){
+        return 'Par'
+    } else{
+        return 'Ímpar'
+    }
+}
+
+//let resultado = parimpar(4)
+//console.log(resultado)
+console.log(parimpar(13))
