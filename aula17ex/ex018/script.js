6dc0695b5825b481981f37ab0fbee1835d6b8537
joao-araujoo <joao.araujoo2007@gmail.com>
2022-10-25 18:37:34 -0300

    código finalizado

--- aula17ex/ex018/script.js
+++ aula17ex/ex018/script.js
@@ -5,12 +5,22 @@ botaoFinalizar.addEventListener('click', finalizar)
 
 let números = []
 
+function estaNaLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    } else{
+        return false
+    }
+}
+
 function adicionar(){
     let txtn = document.querySelector('input#txtn')
     if(txtn.value.length == 0){
         alert('[ERRO] Por favor insira um número!')
     } else if(txtn.value < 1 || txtn.value > 100){
         alert('[ERRO] Insira apenas números entre 1 e 100')
+    } else if(estaNaLista(txtn.value, números) == true){
+        alert('[ERRO] Não insira números repetidos')
     } else{
         let n = Number(txtn.value)
         números.push(n)
@@ -24,7 +34,7 @@ function adicionar(){
 
 function finalizar(){
 
-    let resultado = document.querySelector('p#resultado')
+    let resultado = document.querySelector('div#resultado')
     let numeros_cadastrados = 0
     let s = 0
     números.sort()
