d655a4adbeb586598a22d8257d228f71936abd31
joao-araujoo <joao.araujoo2007@gmail.com>
2022-10-26 14:56:02 -0300

    objeto 2

---
+++ aula17/objeto02.js
@@ -0,0 +1,13 @@
+let cadastro = {
+    nome: '',
+    idade: 0,
+    sexo: '',
+    área: ''
+}
+
+cadastro.nome = 'joão'
+cadastro.idade = 15
+cadastro.sexo = 'masculino'
+cadastro.área = 'front-end'
+
+console.log(`O usuário ${cadastro.nome} é do sexo ${cadastro.sexo}, tem ${cadastro.idade} anos e é da área de ${cadastro.área}`)
