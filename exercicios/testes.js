45250bd392ce2ccab5f193caf7db3a97d25b0a58
joao-araujoo <joao.araujoo2007@gmail.com>
2022-10-28 18:05:08 -0300

    objetos dentro de arrays

---
+++ exercicios/testes.js
@@ -0,0 +1,20 @@
+let países = [
+{
+    nome: 'Brasil',
+    capital: 'Brasília'
+},
+{   
+    nome: 'Estados Unidos',
+    capital: 'Washington'
+},
+{
+    nome: 'México',
+    capital: 'Cidade do México'
+}
+]
+
+let brasil = 0
+let eua = 1
+let mexico = 2
+
+console.log(países[brasil].capital)
