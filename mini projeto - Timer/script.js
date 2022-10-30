3403aadfba033e4dcea21b484c27f79e1ddb4c9d
joao-araujoo <joao.araujoo2007@gmail.com>
2022-10-30 15:19:47 -0300

    arrumando display

--- mini projeto - Timer/script.js
+++ mini projeto - Timer/script.js
@@ -5,3 +5,21 @@ let seconds = time.getSeconds()
 document.querySelector('span#hours').innerHTML = hours
 document.querySelector('span#minutes').innerHTML = minutes
 document.querySelector('span#seconds').innerHTML = seconds
+
+if(seconds < 10){
+    document.querySelector('span#seconds').innerHTML = '0' + seconds
+} else {
+    document.querySelector('span#seconds').innerHTML = seconds
+}
+
+if(minutes < 10){
+    document.querySelector('span#minutes').innerHTML = '0' + minutes
+} else {
+    document.querySelector('span#minutes').innerHTML = minutes
+}
+
+if(hours < 10){
+    document.querySelector('span#hours').innerHTML = '0' + hours
+} else {
+    document.querySelector('span#hours').innerHTML = hours
+}
