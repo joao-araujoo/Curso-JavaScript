81fc166ca44a33e1da7bfe0d2764a103af590cf4
joao-araujoo <joao.araujoo2007@gmail.com>
2022-11-01 16:50:36 -0300

    exercício 07

---
+++ exercicios/exercício07.js
@@ -0,0 +1,10 @@
+//Crie um programa que por meio de uma função, calcule a potência de 2 números
+
+function potência(b=0, e=0){
+    return b**e
+}
+
+let base = 5 //O usuário escolhe
+let expoente = 2 //O usuário escolhe
+
+console.log(potência(base, expoente))
