61ce606f63e4e306c817a095a6594e8182d0d17f
joao-araujoo <joao.araujoo2007@gmail.com>
2022-11-02 14:22:26 -0300

    CSS

--- mini projeto - lâmpada/script.js
+++ mini projeto - lâmpada/script.js
@@ -1,18 +1,15 @@
 let lamp = document.querySelector('img#lamp')
-let buttonOn = document.querySelector('button#on')
-let buttonOff = document.querySelector('button#off')
-let isBroken = false
-
-buttonOn.addEventListener('click', turnLampOn)
-buttonOff.addEventListener('click', turnLampOff)
 lamp.addEventListener('dblclick', brokeLamp)
+document.querySelector('button#on').addEventListener('click', turnLampOn)
+document.querySelector('button#off').addEventListener('click', turnLampOff)
+let isBroken = false
 
 function turnLampOn(){
     isBroken == false ? lamp.src = 'img/ligada.jpg' : alert('Não é possível ligar uma lâmpada quebrada...')
 }
 
 function turnLampOff(){
-    isBroken == false ? lamp.src = 'img/desligada.jpg' : alert('Não é possível ligar uma lâmpada quebrada...')
+    isBroken == false ? lamp.src = 'img/desligada.jpg' : alert('Não é possível desligar uma lâmpada quebrada...')
 }
 
 function brokeLamp(){
