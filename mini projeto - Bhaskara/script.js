f467f17d89b47381544b314056883cbf74aaf5e8
joao-araujoo <joao.araujoo2007@gmail.com>
2022-11-08 17:39:18 -0300

    script avançado

--- mini projeto - Bhaskara/script.js
+++ mini projeto - Bhaskara/script.js
@@ -1,9 +1,28 @@
-function calcular(){
+const deltaMenorQueZero = (d) => d < 0
+
+function calcular() {
     let a = Number(document.querySelector('input#a-value').value)
     let b = Number(document.querySelector('input#b-value').value)
     let c = Number(document.querySelector('input#c-value').value)
-    const delta = (b**2) -4 * a * c
-    const x1 = (-b + Math.sqrt(delta)) / (2 * a)
-    const x2 = (-b - Math.sqrt(delta)) / (2 * a)
-    document.querySelector('div.resultado').innerHTML = `X1 = ${x1} X2 = ${x2}`
+    let delta = (b ** 2) - 4 * a * c
+
+    if(deltaMenorQueZero(delta)){
+        document.querySelector('div.resultado').innerHTML = `X = ∅<br>Nenhuma raíz real`
+        resetar(a, b, c)
+    } else if(delta == 0){
+        const x = -b / (2 * a)
+        document.querySelector('div.resultado').innerHTML = `X = ${x}<br>Uma raíz real`
+        resetar(a, b, c)
+    } else{
+        const x1 = (-b + Math.sqrt(delta)) / (2 * a)
+        const x2 = (-b - Math.sqrt(delta)) / (2 * a)
+        document.querySelector('div.resultado').innerHTML = `X1 = ${x1} X2 = ${x2}<br>Duas raízes reais`
+        resetar(a, b, c)
+    }
+}
+
+function resetar(A, B, C){
+    A = document.querySelector('input#a-value').value = ''
+    B = document.querySelector('input#b-value').value = ''
+    C = document.querySelector('input#c-value').value = ''
 }
