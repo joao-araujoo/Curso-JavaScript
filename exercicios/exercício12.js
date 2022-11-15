6dade73ff0ab80ba0ca23220743de7c70c6bca6c
joao-araujoo <joao.araujoo2007@gmail.com>
2022-11-14 22:12:28 -0300

    exercício 12

---
+++ exercicios/exercício12.js
@@ -0,0 +1,5 @@
+let somar = (x, y) => x + y
+let subtrair = (x, y) => x - y
+let multiplicar = (x, y) => x * y
+let dividir = (x, y) => x / y
+let tudoEmUm = (x, y) => dividir(multiplicar(subtrair(somar(x, y))))
