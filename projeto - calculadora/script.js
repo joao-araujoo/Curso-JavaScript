448b17b6bba2017394b1b7abdb89313947cd2b4f
joao-araujoo <joao.araujoo2007@gmail.com>
2022-11-15 21:35:49 -0300

    clean e backspace

--- projeto - calculadora/script.js
+++ projeto - calculadora/script.js
@@ -1,3 +1,16 @@
 function insert(num){
     document.querySelector('p#resultado').innerHTML += num
 }
+
+function clean(){
+    document.querySelector('p#resultado').innerHTML = ''
+}
+
+function backspace(){
+    let resultado = document.querySelector('p#resultado').innerHTML
+    document.querySelector('p#resultado').innerHTML = resultado.substring(0, resultado.length - 1)
+}
+
+function calcular(){
+
+}
